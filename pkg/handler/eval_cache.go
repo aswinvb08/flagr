0a2a58ad8fbc493834c0eaef1f2b1023932a5d7f
aswinvb08 <aswin_v.b@scaler.com>
2023-07-10 20:08:12 +0530

    init

---
+++ pkg/handler/eval_cache.go
@@ -0,0 +1,162 @@
+package handler
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/openflagr/flagr/swagger_gen/models"
+
+	"github.com/openflagr/flagr/pkg/config"
+	"github.com/openflagr/flagr/pkg/entity"
+	"github.com/openflagr/flagr/pkg/util"
+
+	"github.com/sirupsen/logrus"
+	"github.com/zhouzhuojie/withtimeout"
+)
+
+var (
+	singletonEvalCache     *EvalCache
+	singletonEvalCacheOnce sync.Once
+)
+
+type cacheContainer struct {
+	idCache  map[string]*entity.Flag
+	keyCache map[string]*entity.Flag
+	tagCache map[string]map[uint]*entity.Flag
+}
+
+// EvalCache is the in-memory cache just for evaluation
+type EvalCache struct {
+	cache           atomic.Value
+	refreshTimeout  time.Duration
+	refreshInterval time.Duration
+}
+
+// GetEvalCache gets the EvalCache
+var GetEvalCache = func() *EvalCache {
+	singletonEvalCacheOnce.Do(func() {
+		ec := &EvalCache{
+			refreshTimeout:  config.Config.EvalCacheRefreshTimeout,
+			refreshInterval: config.Config.EvalCacheRefreshInterval,
+		}
+		singletonEvalCache = ec
+	})
+	return singletonEvalCache
+}
+
+// Start starts the polling of EvalCache
+func (ec *EvalCache) Start() {
+	err := ec.reloadMapCache()
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for range time.Tick(ec.refreshInterval) {
+			err := ec.reloadMapCache()
+			if err != nil {
+				logrus.WithField("err", err).Error("reload evaluation cache error")
+			}
+		}
+	}()
+}
+
+func (ec *EvalCache) GetByTags(tags []string, operator *string) []*entity.Flag {
+	var results map[uint]*entity.Flag
+
+	if operator == nil || *operator == models.EvaluationBatchRequestFlagTagsOperatorANY {
+		results = ec.getByTagsANY(tags)
+	}
+
+	if operator != nil && *operator == models.EvaluationBatchRequestFlagTagsOperatorALL {
+		results = ec.getByTagsALL(tags)
+	}
+
+	values := make([]*entity.Flag, 0, len(results))
+	for _, f := range results {
+		values = append(values, f)
+	}
+
+	return values
+}
+
+func (ec *EvalCache) getByTagsANY(tags []string) map[uint]*entity.Flag {
+	results := map[uint]*entity.Flag{}
+	cache := ec.cache.Load().(*cacheContainer)
+
+	for _, t := range tags {
+		fSet, ok := cache.tagCache[t]
+		if ok {
+			for fID, f := range fSet {
+				results[fID] = f
+			}
+		}
+	}
+	return results
+}
+
+func (ec *EvalCache) getByTagsALL(tags []string) map[uint]*entity.Flag {
+	results := map[uint]*entity.Flag{}
+	cache := ec.cache.Load().(*cacheContainer)
+
+	for i, t := range tags {
+		fSet, ok := cache.tagCache[t]
+		if !ok {
+			// no flags
+			return map[uint]*entity.Flag{}
+		}
+
+		if i == 0 {
+			// store all the flags
+			for fID, f := range fSet {
+				results[fID] = f
+			}
+		} else {
+			for fID := range results {
+				if _, ok := fSet[fID]; !ok {
+					delete(results, fID)
+				}
+			}
+
+			// no flags left
+			if len(results) == 0 {
+				return results
+			}
+		}
+	}
+
+	return results
+}
+
+// GetByFlagKeyOrID gets the flag by Key or ID
+func (ec *EvalCache) GetByFlagKeyOrID(keyOrID interface{}) *entity.Flag {
+	s := util.SafeString(keyOrID)
+	cache := ec.cache.Load().(*cacheContainer)
+	f, ok := cache.idCache[s]
+	if !ok {
+		f = cache.keyCache[s]
+	}
+	return f
+}
+
+func (ec *EvalCache) reloadMapCache() error {
+	if config.Config.NewRelicEnabled {
+		defer config.Global.NewrelicApp.StartTransaction("eval_cache_reload", nil, nil).End()
+	}
+
+	_, _, err := withtimeout.Do(ec.refreshTimeout, func() (interface{}, error) {
+		idCache, keyCache, tagCache, err := ec.fetchAllFlags()
+		if err != nil {
+			return nil, err
+		}
+
+		ec.cache.Store(&cacheContainer{
+			idCache:  idCache,
+			keyCache: keyCache,
+			tagCache: tagCache,
+		})
+		return nil, err
+	})
+
+	return err
+}
---
+++ pkg/handler/subject_test.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	jwt "github.com/form3tech-oss/jwt-go"
+	"github.com/openflagr/flagr/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetSubjectFromJWT(t *testing.T) {
+	var ctx context.Context
+
+	defer func() { config.Config.JWTAuthEnabled = false }()
+	config.Config.JWTAuthEnabled = true
+
+	r, _ := http.NewRequest("GET", "", nil)
+	assert.Equal(t, getSubjectFromRequest(r), "")
+
+	ctx = context.TODO()
+	assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "")
+
+	//nolint:staticcheck // jwt-middleware is using the string type of context key
+	ctx = context.WithValue(ctx, config.Config.JWTAuthUserProperty, &jwt.Token{})
+	assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "")
+
+	//nolint:staticcheck // jwt-middleware is using the string type of context key
+	ctx = context.WithValue(ctx, config.Config.JWTAuthUserProperty, &jwt.Token{
+		Claims: jwt.MapClaims{
+			"sub": "foo@example.com",
+		},
+		Valid: true,
+	})
+	assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "foo@example.com")
+}
+
+func TestGetSubjectFromOauthProxy(t *testing.T) {
+	var ctx = context.Background()
+
+	defer func() { config.Config.HeaderAuthEnabled = false }()
+	config.Config.HeaderAuthEnabled = true
+
+	r, _ := http.NewRequest("GET", "", nil)
+	assert.Equal(t, getSubjectFromRequest(r), "")
+
+	r.Header.Set(config.Config.HeaderAuthUserField, "foo@example.com")
+	assert.Equal(t, getSubjectFromRequest(r.WithContext(ctx)), "foo@example.com")
+}
